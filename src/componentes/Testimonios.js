9a9e411f30f4797ed2c0cfb4d9024b18031df3d0
giuli <guly_09_@hotmail.com>
2023-01-15 00:17:25 -0300

    second commit

---
+++ src/componentes/Testimonios.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../stylesheets/Testimonio.css';
+
+function Testimonio(props){ //los props es lo que más caracteriza react
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio' 
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt='foto de Emma' />    
+      <div className='contenedor-texto-testimonio'> 
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'> 
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"</p>
+      </div>  
+    </div>
+  );
+}
+
+export default Testimonio;
